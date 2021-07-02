a9da6fdc0975d47a0dd611ef06aa39ae5ea07d32
John Fuquene <jfuquene@email.wm.edu>
2021-07-02 19:53:03 -0400

    arrays of snake

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
 const scoreDisplay = document.getElementById('score')
+
+let squares = []
+let currentSnake = [2,1,0]
