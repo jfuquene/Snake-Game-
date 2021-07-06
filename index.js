2e5f721995b77ee1abcbdbcfe8af7912b4b702db
John Fuquene <jfuquene@email.wm.edu>
2021-07-05 23:03:49 -0400

    create grid

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ let direction = 1
 const width = 10
 let appleIndex = 0
 let score = 0
+
+function createGrid() {
+    //create 100 of these elements with a for loop
+    for (let i=0; i < width*width; i++) {
+     //create element
+    const square = document.createElement('div')
+    //add styling to the element
+    square.classList.add('square')
+    //put the element into our grid
+    grid.appendChild(square)
+    //push it into a new squares array    
+    squares.push(square)
+    }
+}
+createGrid()
